cb4ff122652878a8ceddf8adf3ce478bf92b4cc9
Vusal Aliyev <vusalaliev02@gmail.com>
2023-07-05 02:00:19 +0400

    Add project files.

---
+++ CRUDwithDapper/Controllers/SuperHeroController.cs
@@ -0,0 +1,36 @@
+﻿using CRUDwithDapper.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+
+namespace CRUDwithDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SuperHeroController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("select * from superheroes");
+            return Ok(heroes);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into superheroes (name, firstname, lastname, city) values (@Name, @FirstName, @LastName, @City)", hero);
+            return Ok();
+        }
+    }
+}
---
+++ CRUDwithDapper/Models/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDwithDapper.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }  
+    }
+}
