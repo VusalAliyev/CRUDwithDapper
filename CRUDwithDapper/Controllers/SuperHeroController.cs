37338b2148855cd42eefbab8647d364f0f6c9eeb
Vüsal İ. Əliyev <Vusal.Eliyev@ABC-Telecom.crp>
2023-07-05 17:37:59 +0400

    Done with Postgre

--- CRUDwithDapper/Controllers/SuperHeroController.cs
+++ CRUDwithDapper/Controllers/SuperHeroController.cs
@@ -2,6 +2,7 @@
 using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 using System.Data.SqlClient;
 using System.Runtime.InteropServices;
 
@@ -12,25 +13,45 @@ namespace CRUDwithDapper.Controllers
     public class SuperHeroController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly NpgsqlConnection _connection;
 
         public SuperHeroController(IConfiguration configuration)
         {
             _configuration = configuration;
+            _connection = new NpgsqlConnection(_configuration.GetConnectionString("DefaultConnection"));
         }
 
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
         {
-            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-            var heroes = await connection.QueryAsync<SuperHero>("select * from superheroes");
+            var heroes = await _connection.QueryAsync<SuperHero>("select * from superheroes");
             return Ok(heroes);
+        }
+        [HttpGet("{heroId}")]
+        public async Task<IActionResult> GetSingleHero(int heroId)
+        {
+            var hero = await _connection.QueryAsync<SuperHero>("Select * from superheroes where Id=@id", new { Id = heroId });
+            return Ok(hero);
         }
         [HttpPost]
         public async Task<IActionResult> CreateHero(SuperHero hero)
         {
-            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-            await connection.ExecuteAsync("insert into superheroes (name, firstname, lastname, city) values (@Name, @FirstName, @LastName, @City)", hero);
+            await _connection.ExecuteAsync("insert into superheroes (name, firstname, lastname, city) values (@Name, @FirstName, @LastName, @City)", hero);
             return Ok(); 
         }
+        [HttpPut]
+        public async Task<IActionResult> UpdateHero(SuperHero hero)
+        {
+            await _connection.ExecuteAsync("update superheroes set name=@Name,firstname=@FirstName, lastname=@LastName, city=@City where id=@Id",hero);
+            return Ok();
+        }
+        [HttpDelete("{heroId}")]
+        public async Task<IActionResult> UpdateHero(int heroId)
+        {
+            await _connection.ExecuteAsync("delete from superheroes where id=@Id", new {Id=heroId});
+            return Ok();
+        }
+
+
     }
 }
